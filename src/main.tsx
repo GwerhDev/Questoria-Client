b603432b04a4194d9594a6ffeb8d0520ef893b3b
Lalo Freak <lalofreak.dev@gmail.com>
2023-10-10 16:59:19 -0300

    redux integrations

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import './styles/root.css';
 import './styles/globals.css';
 import ReactDOM from 'react-dom/client';
 import RoutesApp from './app/routes/index.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import store from './middlewares/redux/store';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <RoutesApp />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <HashRouter>
+      <RoutesApp />
+    </HashRouter>,
+  </Provider>
 )
---
+++ src/middlewares/redux/store.ts
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducer';
+import thunkMiddleware from 'redux-thunk';
+import thunk from 'redux-thunk';
+import { DEVELOPMENT } from '../misc/consts';
+import { environment } from '../../environment';
+
+
+let store = {};
+const composeEnhancer = window.REDUX_DEVTOOLS_EXTENSIONS_COMPOSE || compose;
+
+if(environment === DEVELOPMENT){
+    store = createStore(
+        rootReducer,
+        compose(
+            applyMiddleware(thunk),
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+    );
+} else {
+    store = createStore(
+        rootReducer,
+        composeEnhancer(applyMiddleware(thunkMiddleware))
+    );
+}
+
+export default store;
