fa4a33dbac3b82f231dd062e0c1d8810c64cd438
Lalo Freak <lalofreak.dev@gmail.com>
2023-10-11 15:28:24 -0300

    connecting backend

--- src/middlewares/config/index.ts
+++ src/middlewares/config/index.ts
@@ -1,7 +1,4 @@
 import { environment } from "../../environment";
 import { DEVELOPMENT } from "../misc/consts";
 
-export const URL_API = environment === DEVELOPMENT ? "http://localhost:8080":"https://laruinatv-api.fly.dev";
-
-export const URL_LARUINAHUB_REGISTER = "https://hub.laruinarecords.cl/#/register";
-export const URL_LARUINAHUB_LOGIN = "https://hub.laruinarecords.cl/#/oauth/chooseaccount/tv.laruinarecords.cl";
+export const URL_API = environment === DEVELOPMENT ? "http://localhost:8080":"https://gustavoeduca-api.fly.dev";
