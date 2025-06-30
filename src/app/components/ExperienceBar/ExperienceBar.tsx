5c4cb4d9a7a5c08aa8c13ac985af4f5e416517db
gerardo <gerardo@recemed.com>
2025-06-30 11:35:52 -0400

    adding different experience bar for roles

--- src/app/components/ExperienceBar/ExperienceBar.tsx
+++ src/app/components/ExperienceBar/ExperienceBar.tsx
@@ -1,19 +1,24 @@
-import { useSelector } from 'react-redux';
 import s from './ExperienceBar.module.css';
+import { useSelector } from 'react-redux';
 
 export const ExperienceBar = () => {
   const currentUser = useSelector((state: any) => state.currentUser);
   const experience = currentUser?.experience || 0;
-  const level = currentUser?.level || 1; // Assuming level starts at 1
-  const maxExperienceForLevel = 100; // Placeholder: You'll need a function to calculate this based on level
+  const level = currentUser?.level || 1;
+  const maxExperienceForLevel = 100;
+  const isAdmin = currentUser?.role === "admin";
+  const isTeacher = currentUser?.role === "teacher";
+  const isStudent = currentUser?.role === "student";
 
   const progress = (experience / maxExperienceForLevel) * 100;
 
   return (
     <div className={s.container}>
       <div className={s.barBackground}>
-        <span className={s.text}>Level {level} - Exp: {experience}/{maxExperienceForLevel}</span>
-        <div className={s.barFill} style={{ width: `${progress}%` }}>
+        {isAdmin && <span className={s.text}>Admin</span>}
+        {isTeacher && <span className={s.text}>Master</span>}
+        {isStudent && <span className={s.text}>Level {level} - Exp: {experience}/{maxExperienceForLevel}</span>}
+        <div className={`${s.barFill} ${isTeacher && s.teacherBarFill} ${isAdmin && s.adminBarFill}`} style={{ width: `${(isTeacher || isAdmin) ? 100 : progress}%` }}>
         </div>
       </div>
     </div>
