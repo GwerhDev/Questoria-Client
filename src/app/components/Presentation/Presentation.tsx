9ba44f1a0b1262c705bc2aa1cb3902d73bf19ad0
GwerhDev <gwerh.dev@gmail.com>
2025-06-30 22:05:44 -0400

    cleaning presentation

--- src/app/components/Presentation/Presentation.tsx
+++ src/app/components/Presentation/Presentation.tsx
@@ -3,10 +3,7 @@ import s from './Presentation.module.css'
 export const Presentation = () => {
   return (
     <div className={s.container}>
-      <div className={s.photoContainer}>
-        <img src="" alt="" width="100%"/>
-      </div>
-      <h3>Mr <span>G Fernández</span></h3>
+
     </div>
   )
 }
