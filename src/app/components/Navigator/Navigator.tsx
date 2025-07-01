20645172c79abb7291df8100722d06a0b5166e88
GwerhDev <gwerh.dev@gmail.com>
2025-06-30 22:05:08 -0400

    reordering lateral menu

--- src/app/components/Navigator/Navigator.tsx
+++ src/app/components/Navigator/Navigator.tsx
@@ -2,7 +2,6 @@ import s from './Navigator.module.css';
 import { useSelector } from 'react-redux';
 import { IconButton } from '../Buttons/IconButton';
 import { MenuButton } from '../Buttons/MenuButton';
-import { AuthButton } from '../Buttons/AuthButton';
 import { AccountButton } from '../Buttons/AccountButton';
 import { faBagShopping, faHome, faScroll, faShield, faTableColumns } from '@fortawesome/free-solid-svg-icons';
 
@@ -12,18 +11,10 @@ export const Navigator = () => {
   return (
     <div className={s.container}>
       <nav className={s.navigator}>
-        <section className={s.accountSection}>
-          {currentUser ? <AccountButton /> : <AuthButton />}
-        </section>
         <section className={s.menuSection}>
           <IconButton icon={faHome} to={"/"} title="Inicio" />
-          {
-            currentUser &&
-            <>
-              <IconButton icon={faScroll} to={"/adventures"} title="Aventuras" />
-              <IconButton icon={faShield} to={"/clan"} title="Clan" />
-            </>
-          }
+          <IconButton icon={faScroll} to={"/adventures"} title="Aventuras" />
+          <IconButton icon={faShield} to={"/clan"} title="Clan" />
           <IconButton icon={faBagShopping} to={"/shop"} title={"Tienda"} />
           {(currentUser?.role === "admin" || currentUser?.role === "teacher") && (
             <IconButton icon={faTableColumns} to={"/dashboard"} title={"Dashboard"} />
@@ -31,6 +22,7 @@ export const Navigator = () => {
         </section>
         <section className={s.appSection}>
           <MenuButton />
+          <AccountButton />
         </section>
       </nav>
     </div>
