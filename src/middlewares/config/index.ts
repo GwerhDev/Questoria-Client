9835f690cf5d4456ef09037199042b09a2901598
gerardo <gerardo@recemed.com>
2025-07-01 11:00:15 -0400

    changing api name

--- src/middlewares/config/index.ts
+++ src/middlewares/config/index.ts
@@ -1,4 +1,4 @@
 import { environment } from "../../environment";
 import { DEVELOPMENT } from "../misc/consts";
 
-export const URL_API = environment === DEVELOPMENT ? "http://localhost:8080":"https://GustavoEduca-api.fly.dev";
+export const URL_API = environment === DEVELOPMENT ? "http://localhost:8080":"https://api.questoria.cl";
