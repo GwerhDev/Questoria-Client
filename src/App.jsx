8527d826262eed2d85e506056210b1b562d57999
gerardo <gerardo@recemed.com>
2025-07-31 10:13:25 -0400

    restructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DashboardLayout from './layouts/DashboardLayout';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
 import AccountPage from './pages/AccountPage';
 import AdventurePage from './pages/AdventurePage';
+import UnauthorizedPage from './pages/UnauthorizedPage';
 import AdventureDetailPage from './pages/AdventureDetailPage';
 
 const AppContent = () => {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/unauthorized" element={<UnauthorizedPage />} />
       <Route path="/*" element={
         <DashboardLayout>
           <Routes>
